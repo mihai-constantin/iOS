31132a63eb9739554113bdce219f1bd17f83002a
Mihai Constantin <mconstan@intralinks.com>
2024-05-24 18:17:51 +0300

    update ui tests

--- 6. Swiftful Thinking/Bootcamp/BootcampUI_Tests/UITestingView_UITests.swift
+++ 6. Swiftful Thinking/Bootcamp/BootcampUI_Tests/UITestingView_UITests.swift
@@ -21,14 +21,7 @@ final class UITestingView_UITests: XCTestCase {
     }
 
     func test01_UITestingView_signUpButton_shouldNotSignIn() {
-        // given
-        let textField = app.textFields["SignUpTextField"]
-        
-        // when
-        textField.forceTapElement()
-        
-        app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.forceTapElement()
-        app.buttons["SignUpButton"].forceTapElement()
+        signUpAndSignIn(shouldTypeOnKeyboard: false)
 
         let navBar = app.navigationBars["Welcome"]
         
@@ -37,18 +30,8 @@ final class UITestingView_UITests: XCTestCase {
     }
     
     func test02_UITestingView_signUpButton_shouldSignIn() {
-        // given
-        let textField = app.textFields["SignUpTextField"]
-        
         // when
-        textField.forceTapElement()
-        
-        let keyA = app.keys["A"]
-        let keyAExists = keyA.waitForExistence(timeout: 5)
-        keyA.forceTapElement()
-        
-        app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.forceTapElement()
-        app.buttons["SignUpButton"].forceTapElement()
+        signUpAndSignIn(shouldTypeOnKeyboard: true)
 
         let navBar = app.navigationBars["Welcome"]
         
@@ -58,88 +41,49 @@ final class UITestingView_UITests: XCTestCase {
     
     func test03_SignedInHomeView_showAlertButton_shouldDisplayAlert() {
         // given
-        let textField = app.textFields["SignUpTextField"]
-        
+        signUpAndSignIn(shouldTypeOnKeyboard: true)
+
         // when
-        textField.forceTapElement()
-        
-        app.keys["D"].forceTapElement()
-        
-        app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.forceTapElement()
-        app.buttons["SignUpButton"].forceTapElement()
-        
-        let navBar = app.navigationBars["Welcome"]
-        XCTAssertTrue(navBar.exists)
-        
-        let alertButton = app.buttons["ShowAlertButton"]
-        alertButton.forceTapElement()
-        
-        let alert = app.alerts.firstMatch
+        tapAlertButton(shouldDismissAlert: false)
         
         // then
+        let alert = app.alerts.firstMatch
         XCTAssertTrue(alert.exists)
     }
     
     func test04_SignedInHomeView_showAlertButton_shouldDisplayAndDismissAlert() {
         // given
-        let textField = app.textFields["SignUpTextField"]
+        signUpAndSignIn(shouldTypeOnKeyboard: true)
         
         // when
-        textField.forceTapElement()
-        
-        app.keys["D"].forceTapElement()
-        
-        app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.forceTapElement()
-        app.buttons["SignUpButton"].forceTapElement()
-        
-        let navBar = app.navigationBars["Welcome"]
-        XCTAssertTrue(navBar.exists)
-        
-        let alertButton = app.buttons["ShowAlertButton"]
-        alertButton.forceTapElement()
-        
-        let alert = app.alerts.firstMatch
-        let alertOKButton = alert.buttons["OK"]
-        
-        let alertOKButtonExists = alertOKButton.waitForExistence(timeout: 5)
-        XCTAssertTrue(alertOKButtonExists)
-        
-        alertOKButton.forceTapElement()
+        tapAlertButton(shouldDismissAlert: true)
         
         // then
-        let alertExists = alert.waitForExistence(timeout: 5)
+        let alertExists = app.alerts.firstMatch.waitForExistence(timeout: 5)
+        XCTAssertFalse(alertExists)
     }
     
     func test05_SignedInHomeView_navigationLinkToDestination_shouldNavigateToDestination() {
+        // when
+        signUpAndSignIn(shouldTypeOnKeyboard: true)
         
+        // given
+        tapNavigationLink(shouldDismissDestination: false)
+        
+        // then
+        let destinationText = app.staticTexts["Destination"]
+        XCTAssertTrue(destinationText.exists)
     }
     
     func test06_SignedInHomeView_navigationLinkToDestination_shouldNavigateToDestinationAndGoBack() {
-        
         // given
-        let textField = app.textFields["SignUpTextField"]
+        signUpAndSignIn(shouldTypeOnKeyboard: true)
         
         // when
-        textField.forceTapElement()
-        
-        app.keys["D"].forceTapElement()
-        
-        app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.forceTapElement()
-        app.buttons["SignUpButton"].forceTapElement()
-        
-        let navBar = app.navigationBars["Welcome"]
-        XCTAssertTrue(navBar.exists)
-        
-        let navLinkButton = app/*@START_MENU_TOKEN@*/.buttons["NavigationLinkToDestination"]/*[[".buttons[\"Navigate\"]",".buttons[\"NavigationLinkToDestination\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        navLinkButton.forceTapElement()
-        
-        let destinationText = app.staticTexts["Destination"]
-        XCTAssertTrue(destinationText.exists)
-        
-        let backButton = app.navigationBars.buttons["Welcome"]
-        backButton.forceTapElement()
+        tapNavigationLink(shouldDismissDestination: true)
         
         // then
+        let navBar = app.navigationBars["Welcome"]
         XCTAssertTrue(navBar.exists)
     }
 }
@@ -152,6 +96,48 @@ extension XCUIElement {
         else {
             let coordinate: XCUICoordinate = self.coordinate(withNormalizedOffset: CGVector(dx:0.0, dy:0.0))
             coordinate.tap()
+        }
+    }
+}
+
+// MARK: - FUNCTIONS
+
+extension UITestingView_UITests {
+    func signUpAndSignIn(shouldTypeOnKeyboard: Bool) {
+        let textField = app.textFields["SignUpTextField"]
+        textField.forceTapElement()
+        
+        if shouldTypeOnKeyboard {
+            let keyA = app.keys["A"]
+            keyA.forceTapElement()
+        }
+        
+        app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.forceTapElement()
+        app.buttons["SignUpButton"].forceTapElement()
+    }
+    
+    func tapAlertButton(shouldDismissAlert: Bool) {
+        let alertButton = app.buttons["ShowAlertButton"]
+        alertButton.forceTapElement()
+        
+        if shouldDismissAlert {
+            let alert = app.alerts.firstMatch
+            let alertOKButton = alert.buttons["OK"]
+            
+            let alertOKButtonExists = alertOKButton.waitForExistence(timeout: 5)
+            XCTAssertTrue(alertOKButtonExists)
+            
+            alertOKButton.forceTapElement()
+        }
+    }
+    
+    func tapNavigationLink(shouldDismissDestination: Bool) {
+        let navLinkButton = app.buttons["NavigationLinkToDestination"]
+        navLinkButton.forceTapElement()
+        
+        if shouldDismissDestination {
+            let backButton = app.navigationBars.buttons["Welcome"]
+            backButton.forceTapElement()
         }
     }
 }
